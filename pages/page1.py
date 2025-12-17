1863fabd11d6a2b42445baa513ede45b57cb8c5e
azizah <wafizah02@gmail.com>
2025-12-17 20:54:14 +0700

    Menyelesaikan tugas memasukkan pendahuluan dan metode di page1.

--- pages/page1.py
+++ pages/page1.py
@@ -6,7 +6,6 @@ st.write("Tugas Akhir Praktikum Big Data")
 st.write("Pengambilan data dari tiga saham dalam proyek tugas Praktikum Big Data bertujuan untuk menganalisis dan membandingkan pergerakan harga serta kinerja saham secara komparatif. Data tersebut digunakan untuk menerapkan proses pengolahan Big Data, mulai dari pengumpulan, pembersihan, hingga analisis dan visualisasi data, sehingga mahasiswa dapat memahami pola, tren, dan karakteristik masing-masing saham. Selain itu, proyek ini bertujuan meningkatkan kemampuan pengambilan keputusan berbasis data melalui analisis data saham secara sistematis dan objektif")
 
 st.write("DAFTAR ISI
-
 BAB I Pendahuluan
 BAB II Metode
 BAB IV Implementasi dan Analisis (Streamlit)
