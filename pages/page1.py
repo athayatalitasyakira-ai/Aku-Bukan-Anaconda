1898faf626a3f5f843b4a02aed92506e1a5ae527
azizah <wafizah02@gmail.com>
2025-12-17 21:24:05 +0700

    Menyelesaikan tugas perkenalan di page1.

--- pages/page1.py
+++ pages/page1.py
@@ -11,7 +11,7 @@ st.write("4. Lintang Puji Lestari (021002302001)")
 
 st.write("Pengambilan data dari tiga saham dalam proyek tugas Praktikum Big Data bertujuan untuk menganalisis dan membandingkan pergerakan harga serta kinerja saham secara komparatif. Data tersebut digunakan untuk menerapkan proses pengolahan Big Data, mulai dari pengumpulan, pembersihan, hingga analisis dan visualisasi data, sehingga mahasiswa dapat memahami pola, tren, dan karakteristik masing-masing saham. Selain itu, proyek ini bertujuan meningkatkan kemampuan pengambilan keputusan berbasis data melalui analisis data saham secara sistematis dan objektif")
 
-st.write("Daftar isi : perkenalan, tujuan, daftar isi, data 3 saham, grafik, dan perbandingan 3 saham")
+st.write("Daftar isi : perkenalan, tujuan, data 3 saham, grafik, dan perbandingan 3 saham")
 
 st.write("Data diambil dari daily IDX indices")
 st.write("Alat yang digunakan excel, github, git bash, streamlit, dan anaconda promt")
