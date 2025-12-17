9b02c8a5a92a4aae3d88013d5ef140c4cc1d5e94
azizah <wafizah02@gmail.com>
2025-12-17 20:44:07 +0700

    menyelesaikan tugas memasukkan pendahuluan dan metode di page1.

--- pages/page1.py
+++ pages/page1.py
@@ -2,3 +2,16 @@ import streamlit as st
 
 st.title("Welcome to Aku Bukan Anaconda's Homepage")
 st.write("Tugas Akhir Praktikum Big Data")
+
+st.write("Pengambilan data dari tiga saham dalam proyek tugas Praktikum Big Data bertujuan untuk menganalisis dan membandingkan pergerakan harga serta kinerja saham secara komparatif. Data tersebut digunakan untuk menerapkan proses pengolahan Big Data, mulai dari pengumpulan, pembersihan, hingga analisis dan visualisasi data, sehingga mahasiswa dapat memahami pola, tren, dan karakteristik masing-masing saham. Selain itu, proyek ini bertujuan meningkatkan kemampuan pengambilan keputusan berbasis data melalui analisis data saham secara sistematis dan objektif")
+
+st.write("DAFTAR ISI
+
+BAB I Pendahuluan
+BAB II Metode
+BAB IV Implementasi dan Analisis (Streamlit)
+BAB V Penutup")
+
+st.write("Data diambil dari https://idx.co.id/id/data-pasar/laporan-statistik/digital-statistic/monthly/stock-price-index/daily-idx-indices?filter=eyJ5ZWFyIjoiMjAyNCIsIm1vbnRoIjoiMSIsInF1YXJ0ZXIiOjAsInR5cGUiOiJtb250aGx5In0%3D")
+
+st.write("alat yang digunakan excel, github, stream
