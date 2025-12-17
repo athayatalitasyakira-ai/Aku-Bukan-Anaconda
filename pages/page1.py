0452cd5d11dcd53daa579923eb037f61a62809e4
azizah <wafizah02@gmail.com>
2025-12-17 21:19:57 +0700

    Menyelesaikan tugas perkenalan di page1.

--- pages/page1.py
+++ pages/page1.py
@@ -4,9 +4,10 @@ st.title("Welcome to Aku Bukan Anaconda's Homepage")
 st.write("Tugas Akhir Praktikum Big Data")
 
 st.write("Nama Anggota Kelompok :")
-st.write("1. Athaya Thalita Syakira (021002301012)2. Wafiq Azizah (021002305001) 
-3. Chyntia Indra Lokananta Joe Aleya (021002301002) 
-4. Lintang Puji Lestari (021002302001)
+st.write("1. Athaya Thalita Syakira (021002301012)")
+st.write("2. Wafiq Azizah (021002305001)")
+st.write("3. Chyntia Indra Lokananta Joe Aleya (021002301002)") 
+st.write("4. Lintang Puji Lestari (021002302001)")
 
 st.write("Pengambilan data dari tiga saham dalam proyek tugas Praktikum Big Data bertujuan untuk menganalisis dan membandingkan pergerakan harga serta kinerja saham secara komparatif. Data tersebut digunakan untuk menerapkan proses pengolahan Big Data, mulai dari pengumpulan, pembersihan, hingga analisis dan visualisasi data, sehingga mahasiswa dapat memahami pola, tren, dan karakteristik masing-masing saham. Selain itu, proyek ini bertujuan meningkatkan kemampuan pengambilan keputusan berbasis data melalui analisis data saham secara sistematis dan objektif")
 
