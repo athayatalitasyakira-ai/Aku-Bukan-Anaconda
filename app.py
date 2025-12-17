c84c22ca1ff3cdc5477f4266c98628202c050828
Athaya <athayatalitasyakira@gmail.com>
2025-12-17 14:35:08 +0700

    update pages

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import streamlit as st
 
 pages = [
     st.Page(page="pages/page1.py", title="Home", icon="🐊"),
-    st.Page(page="pages/page2.py", title="Visualisasi Data", icon="🐍"),
-    st.Page(page="pages/page3.py", title="Settings", icon="🐉")
+    st.Page(page="pages/page2.py", title="Informasi", icon="🦕"),
+    st.Page(page="pages/page3.py", title="Grafik", icon="🦚")
+    st.Page(page="pages/page4.py", title="Perbandingan", icon="🐢"
 ]
 
 pg = st.navigation(
