28427d3d33715380e5d99435436d7aaf277881bb
chyntiai <chyntialokananta@gmail.com>
2025-12-19 17:11:29 +0700

    Fix page1 Streamlit code

--- pages/page1.py
+++ pages/page1.py
@@ -13,7 +13,7 @@ st.write("4. Lintang Puji Lestari (021002302001)")
 st.write("⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔ ꒰ ᧔ෆ᧓ ꒱ ⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔⏔")
 
 st.write("Pengambilan data dari tiga saham dalam proyek tugas Praktikum Big Data bertujuan untuk menganalisis dan membandingkan pergerakan harga serta kinerja saham secara komparatif. Data tersebut digunakan untuk menerapkan proses pengolahan Big Data, mulai dari pengumpulan, pembersihan, hingga analisis dan visualisasi data, sehingga mahasiswa dapat memahami pola, tren, dan karakteristik masing-masing saham. Selain itu, proyek ini bertujuan meningkatkan kemampuan pengambilan keputusan berbasis data melalui analisis data saham secara sistematis dan objektif")
-st.write("─────────────────────────୨ৎ────────────────────────────⊹ ࣪ ﹏𓊝﹏𓂁﹏⊹ ࣪ ˖")
+st.write("─────────────────────────୨ৎ────────────────────────────")
 
 st.write("Daftar isi : perkenalan, tujuan, data 3 saham, grafik, dan perbandingan 3 saham")
 
