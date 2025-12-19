48bdf3f387c7b0fa0fbe726d137d70832b99e63a
Athaya <athayatalitasyakira@gmail.com>
2025-12-19 17:59:20 +0700

    page2.py update

--- pages/page2.py
+++ pages/page2.py
@@ -2,6 +2,11 @@ import streamlit as st
 import pandas as pd
 from pathlib import Path
 
+import os
+st.write("Current working directory:", os.getcwd())
+st.write("File exists:", os.path.exists(file_path))
+
+
 st.title("📊 Nilai Saham Perusahaan LQ45")
 
 # =========================
@@ -19,7 +24,8 @@ perusahaan = st.selectbox("Pilih Perusahaan", lq45)
 # Load Excel (AMAN)
 # =========================
 BASE_DIR = Path(__file__).resolve().parent.parent
-file_path = BASE_DIR / "data" / "Data_Saham_Prakbigdata.xlsx"
+file_path = "Data Saham Prakbigdata (1).xlsx"
+
 
 df = pd.read_excel(file_path)
 df.columns = df.columns.str.strip()
